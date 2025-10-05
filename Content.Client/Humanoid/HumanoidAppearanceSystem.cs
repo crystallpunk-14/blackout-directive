2309b6c6a3a130a1b61da5073952b091db684a4c
Ed <edwardxperia2000@gmail.com>
2025-10-05 15:39:48 +0300

    human species

--- Content.Client/Humanoid/HumanoidAppearanceSystem.cs
+++ Content.Client/Humanoid/HumanoidAppearanceSystem.cs
@@ -111,8 +111,19 @@ public sealed class HumanoidAppearanceSystem : SharedHumanoidAppearanceSystem
         if (protoId == null)
             return;
 
+        //Blackout female bodies support
+        //if (sexMorph)
+        //    protoId = HumanoidVisualLayersExtension.GetSexMorph(key, component.Sex, protoId);
+
         if (sexMorph)
-            protoId = HumanoidVisualLayersExtension.GetSexMorph(key, component.Sex, protoId);
+        {
+            var tempProto = HumanoidVisualLayersExtension.GetSexMorph(key, component.Sex, protoId);
+            if (_prototypeManager.TryIndex<HumanoidSpeciesSpriteLayer>(tempProto, out _))
+            {
+                protoId = tempProto;
+            }
+        }
+        //Blackout female bodies support end
 
         var proto = _prototypeManager.Index<HumanoidSpeciesSpriteLayer>(protoId);
         component.BaseLayers[key] = proto;
